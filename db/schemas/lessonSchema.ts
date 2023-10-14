037d550cd9d04ab45c287741311345e04a2d28ed
rayasalem <rayasalem483@gmail.com>
2023-10-14 16:15:05 +0300

    add quiz and qustion and quiztasker model

--- db/schemas/lessonSchema.ts
+++ db/schemas/lessonSchema.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document, Types } from 'mongoose';
+import { Schema, model,Model, Document, Types } from 'mongoose';
 
 export interface ILesson extends Document {
   title: string;
@@ -26,6 +26,6 @@ const lessonSchema: Schema = new Schema<ILesson>({
   },
 });
 
-const Lesson = model<ILesson>('Lesson', lessonSchema);
+const Lesson:Model<ILesson> = model<ILesson>('Lesson', lessonSchema);
 
 export default Lesson;
